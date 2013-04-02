81e39883a880721213810a3708267f6fff5e6e4b
Michael Little <bio595@gmail.com>
2013-04-03 00:46:07 +1300

    Alerts toggle visibility, logic displaying each alert is broken

--- waterGrapher/public/javascripts/main.js
+++ waterGrapher/public/javascripts/main.js
@@ -20,29 +20,40 @@ $(function() {
 	};
 
 	var currentTime = 7;
+	var currentTotal = 0;
 
 	var plot = $.plot($("#chartContainer"), data, options);
 
-
 	$("#addButton").click(function (e) {
 		e.preventDefault();
-
+		console.log("Curent time: " + currentTime + "\nCurrent total: " + currentTotal);
 		//Get inputs
 		var cumulativeTotal = $("#total").val();
 		var time = $("#timeSelect").val();
-		console.log(time);
+		
 		if(time <= currentTime){
-			alert("Show error alert");
+			console.log("BAD TIME\nCurrent time: " + currentTime + "\nCurrent total: " + currentTotal);
+			$("#badTime").show();
+			return;
+
+		}else if(cumulativeTotal < currentTotal){
+			console.log("BAD TOTAL\nCurrent time: " + currentTime + "\nCurrent total: " + currentTotal);
+			$("#badTotal").show();
 			return;
 		}
+
+		currentTotal = cumulativeTotal;
+		currentTime = time;
 
 		var newCoordinate = [time, cumulativeTotal];
 		data[1].push(newCoordinate);
 
-		console.log(data);
-
 		plot.setData(data);
 		plot.draw();
+	});
+
+	$(".alert .close").click(function (e) {
+		$(e.target).parent().hide();
 	});
 
 });

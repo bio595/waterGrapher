008db367beaa1c3888e0143a55a6143108af6f0f
Michael Little <bio595@gmail.com>
2013-04-02 20:33:04 +1300

    Added local jquery, created basic flot chart

--- waterGrapher/public/javascripts/main.js
+++ waterGrapher/public/javascripts/main.js
@@ -1,5 +1,25 @@
 $(function() {
 
+
+	var data = [
+		[[0,0], [1,1]], //Series 1
+		[[0,0], [2,1]] //Series 2
+	];
+
+	var options = {
+		series: {
+			lines: {
+				fill: true
+			}
+		},
+		yaxis: {
+			max: 5
+		}
+	};
+
+	$.plot($("#chartContainer"), data, options);
+
+
 	$("#addButton").click(function (e) {
 		
 	});

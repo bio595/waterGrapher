f56a1e01bee03f0318eaaf6e9c442c05b301c381
Michael Little <bio595@gmail.com>
2013-04-03 17:52:46 +1300

    Added api route for getting points for a day, not implemented though

--- waterGrapher/app.js
+++ waterGrapher/app.js
@@ -29,6 +29,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/plots/:day', routes.pointsForDay);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- waterGrapher/routes/index.js
+++ waterGrapher/routes/index.js
@@ -4,5 +4,18 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index');
+};
+
+/*
+ * PUBLIC API FROM HERE
+ *
+ *
+ *
+ *	GET points for a specific day 
+ */
+
+exports.pointsForDay = function(req, res){
+	console.log(req.params.day);
+	res.send([[8, 0], [22,5]]);
 };

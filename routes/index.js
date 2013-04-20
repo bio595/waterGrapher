2a46aec28d0486e081f0daeebcbd09e4f8ec4f48
Michael Little <bio595@gmail.com>
2013-04-20 23:31:14 +1200

    added dependencies for mongodb in package.json and set uri to mongodb in routing file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
 
+//Imports
+var mongo = require('mongodb');
+
+//URI for mongodb
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/mydb';
+
 /*
  * GET home page.
  */
@@ -14,7 +22,7 @@ exports.index = function(req, res){
  *
  *	GET points for a specific date in the format yyyy-mm-dd
  */
- 
+
 exports.pointsForDate = function(req, res){
 	console.log(req.params.year + "-" + req.params.month + "-" + req.params.day);
 	res.send([[8, 0], [22,5]]);

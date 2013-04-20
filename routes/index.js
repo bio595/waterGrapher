708c9e0602cd0b3fd4c2eb4a49d4874a95c77903
Michael Little <bio595@gmail.com>
2013-04-20 22:34:41 +1200

    Fixed a merge bug in routing file

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+/*
+ * PUBLIC API FROM HERE
+ *
+ *
+ *
+ *	GET points for a specific date in the format yyyy-mm-dd
+ */
+ 
+exports.pointsForDate = function(req, res){
+	console.log(req.params.year + "-" + req.params.month + "-" + req.params.day);
+	res.send([[8, 0], [22,5]]);
+};

d72a3eb45d76b353cff9b185f9aec14191f45627
Michael Little <bio595@gmail.com>
2013-06-15 12:49:33 +1200

    got flot to show target series

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,6 @@ $(function () {
 	$('a[data-toggle="tab"]').on('shown', function (e) {
 	  
 	  if(e.target.id === "todayTab"){
-	  	//If today isnt already loaded, load it.
 	  	loadToday();	
 	  }else if(e.target.id === "historyTab"){
 	  	loadHistory();
@@ -12,9 +11,20 @@ $(function () {
 	});
 
 
-	loadToday();
+	//Show spinner while we fetch data
+	$('#spinnerContainer').spin();
+
+	//Get information for today
+	var data = loadToday();
+
+	//with our data plot some stuff
+	$.plot($("#placeholder"), data, { xaxis: { min: 0, max: 24},
+									  yaxis: { min: 0, max: 6}
+									});
 
 	function loadToday () {
+		return [ {data: [ [0, 0], [24, 5] ], lines: { fill: true, fillColor: "rgba(255, 0, 0, 0.25)"}, color: "rgba(255, 0, 0, 1.0)" }
+				 ];
 	}
 
 	function loadHistory () {
